fe158f92658bc9dff0b7932d77adec3adfa4c24a
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2022-12-07 20:26:51 -0500

    Add Function Operate
    The function Operate will call other functions depending on
    their switch cases.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function addNumbers(Num1, Num2){
+function addNumbers(Num1, Num2) {
     let result = Num1 + Num2;
     return result;
 }
@@ -6,7 +6,7 @@ function addNumbers(Num1, Num2){
 addNumbers(1, 5);
 
 
-function subtractNumbers(Num1, Num2){
+function subtractNumbers(Num1, Num2) {
     let result = Num1 - Num2;
     return result;
 }
@@ -14,16 +14,59 @@ function subtractNumbers(Num1, Num2){
 subtractNumbers(8, 5);
 
 
-function multiplyNumbers(Num1, Num2){
+function multiplyNumbers(Num1, Num2) {
     let result = Num1 * Num2;
     return result;
 }
 
 multiplyNumbers(4, 2);
 
-function divideNumbers(Num1, Num2){
+function divideNumbers(Num1, Num2) {
     let result = Num1 / Num2;
     return result;
 }
 
 divideNumbers(10, 5);
+
+
+function operate(operator, Num1, Num2) {
+    switch (operator) {
+        case '+':
+            addNumbers(Num1, Num2);
+            break;
+        case '-':
+            subtractNumbers(Num1, Num2);
+            break;
+        case '*':
+            multiplyNumbers(Num1, Num2);
+            break;
+        case '/':
+            divideNumbers(Num1, Num2);
+            break;
+        default:
+            alert("No Valid Input");
+    }
+
+}
+
+
+// function operate(operator, Num1, Num2){
+//     while(operator!=`Esc`){
+//         switch(operator){
+//             case '+':
+//                 addNumbers(Num1, Num2);
+//                 break;
+//             case '-':
+//                 subtractNumbers(Num1, Num2);
+//                 break;
+//             case '*':
+//                 multiplyNumbers(Num1, Num2);
+//                 break;
+//             case '/':
+//                 divideNumbers(Num1, Num2);
+//                 break;
+//             default:
+//                 alert("No Valid Input");
+//         }
+//     }
+// }
