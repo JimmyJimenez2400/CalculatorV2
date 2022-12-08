07089a4692cfd9db32c475daab59b16b888e74b6
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2022-12-07 19:33:37 -0500

    Add Four Functions For Calculator
    Add four functions which were:
    -Addition
    -Subtraction
    -Multiplication
    -Division

---
+++ app.js
@@ -0,0 +1,29 @@
+function addNumbers(Num1, Num2){
+    let result = Num1 + Num2;
+    return result;
+}
+
+addNumbers(1, 5);
+
+
+function subtractNumbers(Num1, Num2){
+    let result = Num1 - Num2;
+    return result;
+}
+
+subtractNumbers(8, 5);
+
+
+function multiplyNumbers(Num1, Num2){
+    let result = Num1 * Num2;
+    return result;
+}
+
+multiplyNumbers(4, 2);
+
+function divideNumbers(Num1, Num2){
+    let result = Num1 / Num2;
+    return result;
+}
+
+divideNumbers(10, 5);
