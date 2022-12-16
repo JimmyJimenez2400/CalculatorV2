15e14da6023d89996facbaf6ae44073cb7ce706e
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2022-12-16 17:50:46 -0500

    Clean Tree

--- app.js
+++ app.js
@@ -20,13 +20,6 @@ numberButtons.forEach((button) => {
     button.addEventListener('click', displayValueToScreen);
 })
 
-// equalButton.addEventListener('click', EqualButtonClicked);
-
-function EqualButtonClicked(Num1, operator, Num2) {
-    let result = Num1 + operator + Num2;
-    return result;
-}
-
 function displayValueToScreen(event) {
     displayValue += event.currentTarget.textContent;
     displayElement.textContent = displayValue;
@@ -102,23 +95,15 @@ function addNumbers(Num1, Num2) {
     return result;
 }
 
-addNumbers(1, 5);
-
-
 function subtractNumbers(Num1, Num2) {
     let result = Num1 - Num2;
     return result;
 }
 
-subtractNumbers(8, 5);
-
-
 function multiplyNumbers(Num1, Num2) {
     let result = Num1 * Num2;
     return result;
 }
-
-multiplyNumbers(4, 2);
 
 function divideNumbers(Num1, Num2) {
     let result = Num1 / Num2;
@@ -144,30 +129,3 @@ function operate(Num1, operator, Num2) {
             alert("No Valid Input");
     }
 }
-
-
-
-
-
-
-
-// function operate(operator, Num1, Num2){
-//     while(operator!=`Esc`){
-//         switch(operator){
-//             case '+':
-//                 addNumbers(Num1, Num2);
-//                 break;
-//             case '-':
-//                 subtractNumbers(Num1, Num2);
-//                 break;
-//             case '*':
-//                 multiplyNumbers(Num1, Num2);
-//                 break;
-//             case '/':
-//                 divideNumbers(Num1, Num2);
-//                 break;
-//             default:
-//                 alert("No Valid Input");
-//         }
-//     }
-// }
