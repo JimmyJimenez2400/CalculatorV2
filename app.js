76a3a3baa6ead9593df6e99bbc7414a7d0a183f2
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2022-12-18 05:06:39 -0500

    Add Function ClearAllContents, Erases Variables To Be Fresh Start

--- app.js
+++ app.js
@@ -1,117 +1,7 @@
-// const displayElement = document.querySelector('.displayNumber');
-// const numberButtons = document.querySelectorAll('.numbers');
-// const operatorButtons = document.querySelectorAll('.operator');
-// const equalButton = document.querySelector('#equal');
-
-// let displayValue = "";
-// let firstOperand = "";
-// let operation = "";
-// let secondOperand = "";
-// let result = 0;
-
-// // equalButton.addEventListener('click', equalButtonClicked);
-
-// operatorButtons.forEach((operator) => {
-//     operator.addEventListener('click', storingValueAndOperation);
-// })
-
-// numberButtons.forEach((button) => {
-//     button.addEventListener('click', displayValueToScreen);
-// })
-
-// function displayValueToScreen(event) {
-//     displayValue += event.currentTarget.textContent;
-//     displayElement.textContent = displayValue;
-// }
-
-
-
-// function equalButtonClicked() {
-//     alert("Pressed Equal");
-//     secondOperand = displayValue;
-//     result = operate(+firstOperand, operation, +secondOperand);
-//     firstOperand = result; 
-//     displayElement.textContent = firstOperand;
-//     operation = "";
-// }
-
-
-// // function storingValueAndOperation(event) {
-// //     if (operation === "") {
-// //         firstOperand = displayValue;
-// //         operation = event.currentTarget.textContent;
-// //         displayValue = "";
-// //     } else {
-// //         secondOperand = displayValue;
-// //         result = operate(+firstOperand, operation, +secondOperand);
-// //         firstOperand = result;
-// //         displayElement.textContent = firstOperand;
-// //     }
-// // }
-
-// /*
-
-// Problem: How can I chain more numbers to the calculator? 
-
-// Breakdown: We need to grab firstOperand, then operation, then secondOperand, then call Operation again but this time we want to actually call the function
-// to operate firstOperand and secondOperand to give us our result
-
-// Plan:
-// Yes, there is an interface. Looks like a display of what a calculator has
-// Functionality the interface would have is that it will use the operators to
-// calculate the result
-
-// Inputs program will have is button pressing
-
-// Desired output is that to get result of first and second Operands
-// depending on operator
-
-// The steps is what I am trying to find out:
-// 1. When given input for operands and operation, we want to
-// to save a second operator after that
-// 2.  which gets the result of the 
-// (firstOperand (operator) secondOperand)
-// 3.  stores secondOperand in this case, since firstOperand will be
-// result of previous operation(step 1)
-
-// Example: 
-// "1 + 2" -> 3, 
-// What happens if I want to do another operator and secondOperand?
-// First, let's tackle by operating the two numbers we have, 1 + 2 turns into 3. Okay, how? Well we need to call function operate 
-// Options:
-// * we could repeat the process, by doing an else if then else. Where else, would keep grabbing the operator and secondOperand
-
-
-// Pseudocode:
-// if operation is clicked
-// operate first pair (1 + 2), save result into global variable result, then save it in firstOperand or displayValue
-// save new operation
-// save secondOperand
-// repeat
-
-// */
-// // function storingValueAndOperation(event) {
-// //     if (operation === "") {
-// //         firstOperand = displayValue;
-// //         operation = event.currentTarget.textContent;
-// //         displayValue = "";
-// //     }
-// //     else{
-// //         secondOperand = displayValue;
-// //         result = operate(+firstOperand, operation, +secondOperand);
-// //         firstOperand = result;
-// //         displayElement.textContent = firstOperand;
-// //         operation = event.currentTarget.textContent;
-// //         displayValue = "";
-// //     }
-// // }
-
-
-
 const displayElement = document.querySelector('.displayNumber');
 const numberButtons = document.querySelectorAll('.numbers');
 const operatorButtons = document.querySelectorAll('.operator');
-const equalButton = document.querySelector('#equal');
+const clearButton = document.querySelector("#clear");
 
 let displayValue = "";
 let firstOperand = "";
@@ -120,7 +10,7 @@ let secondOperand = "";
 let result = 0;
 
 
-
+clearButton.addEventListener('click', clearAllContent);
 
 operatorButtons.forEach((operator) => {
     operator.addEventListener('click', storingValueAndOperation);
@@ -137,22 +27,32 @@ function displayValueToScreen(event) {
     displayElement.textContent = displayValue;
 }
 
+function clearAllContent(event){
+    console.log("You're pressing me!");
+    displayValue = "";
+    firstOperand = "";
+    secondOperand = "";
+    result = 0;
+    storedOperation = "";
+    displayElement.textContent = ""
+    currentOperation = "";
+}
+
 function storingValueAndOperation(event) {
     const currentOperation = event.currentTarget.textContent;
-    if(storedOperation === "" && currentOperation != "=") {
+    if (storedOperation === "" && currentOperation != "=") {
         firstOperand = displayValue;
         storedOperation = currentOperation;
         firstOperand = displayValue;
         displayValue = "";
         console.log(firstOperand);
         console.log(storedOperation);
-    }else if(currentOperation === "=" && firstOperand != "" && storedOperation != ""){
+    } else if (currentOperation === "=" && firstOperand != "" && storedOperation != "") {
         console.log('Calculate Now!');
         secondOperand = displayValue;
         result = operate(+firstOperand, storedOperation, +secondOperand);
         displayElement.textContent = result.toFixed(2);
-    }
-    else if(currentOperation !="="){
+    } else if (currentOperation != "=") {
         console.log("WE ARE HERE!");
         secondOperand = displayValue;
         result = operate(+firstOperand, storedOperation, +secondOperand);
